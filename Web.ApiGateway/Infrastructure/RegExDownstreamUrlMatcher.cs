c1673ae0ecd6d186a976c70a9efbf47257a67eb9
Steve Bering <stevebering@gmail.com>
2020-02-19 06:02:52 -0800

    additional changes to matcher

---
+++ Web.ApiGateway/Infrastructure/RegExDownstreamUrlMatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Ocelot.Configuration;
+using Ocelot.DownstreamRouteFinder.UrlMatcher;
+using Ocelot.Responses;
+using Ocelot.Values;
+
+namespace Web.ApiGateway.Infrastructure
+{
+    public interface IDownstreamUrlPathToTeUrlTemplateMatcher
+    {
+        public Response<UrlMatch> Match(string downstreamUrlPath, string downstreamQueryString, DownstreamReRoute reroute);
+    }
+
+    public class RegExDownstreamUrlMatcher : IDownstreamUrlPathToTeUrlTemplateMatcher
+    {
+        public Response<UrlMatch> Match(string downstreamUrlPath, string downstreamQueryString, DownstreamReRoute reroute)
+        {
+            
+            return pathTemplate.Pattern.IsMatch($"{downstreamUrlPath}{downstreamQueryString}")
+                ? new OkResponse<UrlMatch>(new UrlMatch(true))
+                : new OkResponse<UrlMatch>(new UrlMatch(false));
+        }
+    }
+}
